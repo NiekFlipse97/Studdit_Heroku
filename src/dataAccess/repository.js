1b86f4515472ab74815ca6ab7eff450a464a2c27
Kelly Zegelaar <KNZegelaar@outlook.com>
2018-11-25 15:27:46 +0100

    created the repository.js file

---
+++ src/dataAccess/repository.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+//const user = require(''); path to user goes here <-
+
+function createUser(firstName, lastName, email, password){
+    // const user = new User({})
+
+    // user.save()
+    //      .then()
+    //      .catch()
+};
+
+function findUser(email, password){
+
+};
+
